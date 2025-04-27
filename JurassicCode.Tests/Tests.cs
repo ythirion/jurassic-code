b58cf7102da164db680bc8b3488e2a70516ea0c1
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2025-04-27 15:45:02 +0200

    feat: upgrade target framework to .NET 9.0 and update package references across projects

--- JurassicCode.Tests/Tests.cs
+++ JurassicCode.Tests/Tests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using JurassicCode.Db2;
 using Xunit;
@@ -25,7 +27,7 @@ namespace JurassicCode.Tests
             _parkService.AddDinosaurToZone("Test Zone 1", new Dinosaur { Name = "TestDino2", Species = "Velociraptor", IsCarnivorous = true });
 
             // Test moving a dinosaur to a closed zone (should fail)
-            Action moveToClosedZone = () => _parkService.MoveDinosaur("Test Zone 1", "Test Zone 3", "TestDino1");
+            System.Action moveToClosedZone = () => _parkService.MoveDinosaur("Test Zone 1", "Test Zone 3", "TestDino1");
             moveToClosedZone.Should().Throw<Exception>().WithMessage("Zones are closed or do not exist.");
 
             // Open the second zone and move the dinosaur
@@ -50,7 +52,7 @@ namespace JurassicCode.Tests
 
             // Test adding a dinosaur to a closed zone (should fail)
             _parkService.ToggleZone("Test Zone 1");
-            Action addToClosedZone = () => _parkService.AddDinosaurToZone("Test Zone 1", new Dinosaur { Name = "TestDino3", Species = "Triceratops", IsCarnivorous = false });
+            System.Action addToClosedZone = () => _parkService.AddDinosaurToZone("Test Zone 1", new Dinosaur { Name = "TestDino3", Species = "Triceratops", IsCarnivorous = false });
             addToClosedZone.Should().Throw<Exception>().WithMessage("Zone is closed or does not exist.");
         }
 
@@ -103,7 +105,7 @@ namespace JurassicCode.Tests
             zone1Dinosaurs.Should().NotContain(d => d.Name == "DinoC");
 
             // Test moving a dinosaur to a non-existent zone (should fail)
-            Action moveToNonExistentZone = () => _parkService.MoveDinosaur("Test Zone 4", "Non-Existent Zone", "DinoC");
+            System.Action moveToNonExistentZone = () => _parkService.MoveDinosaur("Test Zone 4", "Non-Existent Zone", "DinoC");
             moveToNonExistentZone.Should().Throw<Exception>().WithMessage("Zones are closed or do not exist.");
         }
     }
